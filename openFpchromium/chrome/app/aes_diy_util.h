c6b0d8ccbc29cf033045d86fba5d7724861e33b1
fpEscort <39473597+FPEsocrter@users.noreply.github.com>
2024-05-09 13:47:51 +0800

    删除历史版本记录，初始化仓库

---
+++ openFpchromium/chrome/app/aes_diy_util.h
@@ -0,0 +1,63 @@
+#ifndef CHROME_APP_AES_DIY_UITL_H_
+#define CHROME_APP_AES_DIY_UITL_H_
+
+#include "base/base64.h"
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
+
+
+void decrypt_aes_cbc(const uint8_t* encrypted_text,
+                     int encrypted_text_len,
+                     const uint8_t* key,
+                     uint8_t* iv,
+                     uint8_t* decrypted_text) {
+  AES_KEY aes_key;
+  AES_set_decrypt_key(key, 128, &aes_key);
+  AES_cbc_encrypt(encrypted_text, decrypted_text, encrypted_text_len, &aes_key,
+                  iv, AES_DECRYPT);
+}
+
+
+std::vector<uint8_t> pKCS5Padding(const std::vector<uint8_t>& ciphertext, int blockSize) {
+    int padding = blockSize - ciphertext.size() % blockSize;
+    std::vector<uint8_t> padText(padding, static_cast<uint8_t>(padding));
+    std::vector<uint8_t> paddedCiphertext = ciphertext;
+    paddedCiphertext.insert(paddedCiphertext.end(), padText.begin(), padText.end());
+    return paddedCiphertext;
+}
+
+std::string diyEncrypt(const std::string& text, const std::string& aesKey) {
+  
+        const uint8_t* key = reinterpret_cast<const uint8_t*>(aesKey.data());
+
+        // Generate a random IV (Initialization Vector)
+        uint8_t iv[AES_BLOCK_SIZE];
+        RAND_bytes(iv, AES_BLOCK_SIZE);
+
+        // Convert the input text to uint8_t vector
+        std::vector<uint8_t> plaintext = pKCS5Padding(std::vector<uint8_t>(text.begin(), text.end()), AES_BLOCK_SIZE);
+        
+        int plaintext_len = plaintext.size();
+
+        std::vector<uint8_t> ciphertext(plaintext_len + AES_BLOCK_SIZE);
+
+        // Copy the IV to the beginning of the ciphertext
+        std::copy(iv, iv + AES_BLOCK_SIZE, ciphertext.begin());
+
+        // Create AES context with PKCS5Padding
+        AES_KEY aes_key;
+        AES_set_encrypt_key(key, 128, &aes_key);
+
+        // Encrypt the plaintext using AES in CBC mode with PKCS5Padding
+        AES_cbc_encrypt(plaintext.data(), &ciphertext[AES_BLOCK_SIZE],
+                        plaintext_len,
+                        &aes_key, iv, AES_ENCRYPT);
+
+        // Convert the ciphertext to a base64-encoded string
+        std::string reStr = base::Base64Encode(ciphertext);
+        return reStr;
+    
+}
+#endif  // CHROME_APP_AES_DIY_UITL_H_
---
+++ openFpchromium/third_party/webrtc/pc/fp_web_rtc.h
@@ -0,0 +1,18 @@
+
+#ifndef PC_FP_WEB_RTC_H_
+#define PC_FP_WEB_RTC_H_
+
+#include "rtc_base/socket_address.h"
+#include "rtc_base/ip_address.h"
+#include "rtc_base/string_encode.h"
+#include "p2p/base/ice_transport_internal.h"
+#include "third_party/blink/public/common/fingerprint/fingerprint.h"
+#include "base/singleton_fingerprint.h"
+
+
+cricket::Candidates FpWebRtc(const cricket::Candidates& candidates){
+   
+   //todo 带优化
+   
+}
+#endif // PC_FP_WEB_RTC_H_
